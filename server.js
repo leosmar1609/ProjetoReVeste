993e8c708516c1a3e292c9cd5928d77482be9c13
Leonardo Marcos <leo.marcos6440@gmail.com>
2025-05-21 21:58:39 -0300

    attServer

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
