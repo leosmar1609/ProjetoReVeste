fb6ff32fabd18cf3488a3c694d79d455894fa908
Leonardo Marcos <leo.marcos6440@gmail.com>
2025-05-22 00:39:40 -0300

    tste

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -10,8 +10,10 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-     
+    database: process.env.DB_NAME,
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 function autenticarToken(req, res, next) {
