36e53bbfa349519260a257140085e37fef93ca8f
Leonardo Marcos <leo.marcos6440@gmail.com>
2025-06-18 20:13:07 -0300

    versionAnt

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
-import fs from 'fs';
 dotenv.config();
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
   ssl: {
-    ca: fs.readFileSync('./certs/isrgrootx1.pem')
+    rejectUnauthorized: false
   }
 });
 
-export default pool;
+export default db;
